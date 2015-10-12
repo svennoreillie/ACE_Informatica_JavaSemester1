f28fb58a3ade70cf3b1063f43953e698bc2bcdb3
Andre <Andre@192.168.0.240>
2015-10-12 21:10:38 +0200

    Fixed DateGreg constructors, plus DateInt() constructor

--- Opdracht1/src/application/App.java
+++ Opdracht1/src/application/App.java
@@ -1,54 +1,12 @@
 package application;
 
-import model.V2.DateGreg;;
+import model.V2.DateGreg;
+import model.V1.*;
 
 public class App {
 
 	public static void main(String[] args) throws Exception {
 
-		////////////////////// WORKING //////////////////////
-		/*
-		// All constructors OK
-		DateGreg test = new DateGreg();
-		System.out.print(test.toString() + "\n");
 		
-		DateGreg test2 = new DateGreg(27, 07, 1990);
-		System.out.print(test2.toString() + "\n");
-		
-		DateGreg test3 = new DateGreg(test);
-		System.out.print(test3.toString() + "\n");
-		
-		DateGreg test4 = new DateGreg("24/08/1998");
-		System.out.print(test4.toString() + "\n\n");
-		
-		
-		// Methods
-		System.out.print(test.getFormatAmerican() + "\n");
-		System.out.print(test.getFormatEuropean() + "\n\n");
-		
-		System.out.print(test.compareTo(test) + "\n");
-		System.out.print(test.smallerThan(test2)  + "\n\n");
-		
-		test.alterDate(-365);
-		System.out.print(test.toString() + "\n");
-		
-		test.changeDate(365);
-		System.out.print(test.toString() + "\n\n");
-		
-		System.out.print(test.compareTo(test2) + "\n\n");
-		*/
-		
-		////////////////////// NOT WORKING //////////////////////
-		
-		//System.out.print(test2.equals(test2) + "\n");
-		//System.out.print(test2.equals(test3) + "\n");
-		
-
-		//test.alterDate(aantalDagen);
-		
-		DateGreg test5 = new DateGreg(29, 02, 2016);
-		DateGreg test6 = new DateGreg("29/02/2016");
-		DateGreg test7 = new DateGreg(01,01,2016);
-		System.out.print(test5.toString() + "\n\n" + test6.toString() + "\n\n" + test7.leapTest());
 	}
 }
