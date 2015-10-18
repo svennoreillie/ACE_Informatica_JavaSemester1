0aa653dde123409d69f88a055b7cd69137ca98da
Andre <Andre@10.100.8.37>
2015-10-18 16:10:11 +0200

    Errors in code not shown? ffs

--- Opdracht1/src/application/App.java
+++ Opdracht1/src/application/App.java
@@ -6,32 +6,9 @@ import model.V1.DateInt;
 public class App {
 
 	public static void main(String[] args) throws Exception {
-		// WORKING
-		DateGreg test = new DateGreg();
-		System.out.print(test.toString() + "\n");
+		DateGreg test1 = new DateGreg();
+		DateGreg test2 = new DateGreg("18/10/2016");
 		
-		
-		DateGreg test2 = new DateGreg(27, 07, 1990);
-		System.out.print(test2.toString() + "\n");
-		
-		
-		DateGreg test3 = new DateGreg(test);
-		System.out.print(test3.toString() + "\n");
-		
-		
-		DateGreg test4 = new DateGreg("24/08/1998");
-		System.out.print(test4.toString() + "\n\n");
-		
-		System.out.print(test.getFormatAmerican() + "\n");
-		System.out.print(test.getFormatEuropean() + "\n\n");
-		
-		System.out.print(test.compareTo(test) + "\n");
-		System.out.print(test.smallerThan(test2)  + "\n\n");
-		
-		// NOT WORKING
-		System.out.print(test2.equals(test2) + "\n");
-		System.out.print(test2.equals(test3) + "\n");
-		
-		//test.alterDate(aantalDagen);
+		System.out.println(test1.differenceInDays(test2));
 	}
 }
