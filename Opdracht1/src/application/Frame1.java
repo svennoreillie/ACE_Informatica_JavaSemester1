40d2c55d7e7d20fd4bad111806b7bcb039fad4a2
Andre <Andre@192.168.0.240>
2015-11-01 13:24:50 +0100

    Default date for datepicker

--- Opdracht1/src/application/Frame1.java
+++ Opdracht1/src/application/Frame1.java
@@ -16,10 +16,12 @@ import com.toedter.calendar.JDateChooser;
 import model.DateFactory;
 
 import java.util.List;
+import java.util.Calendar;
 import java.util.Date;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.time.LocalDateTime;
 
 public class Frame1 extends JFrame {
 
@@ -82,34 +84,13 @@ public class Frame1 extends JFrame {
 		JLabel lblNumberOfNights = new JLabel("Number of nights :");
 		lblNumberOfNights.setBounds(24, 55, 95, 14);
 		frame.getContentPane().add(lblNumberOfNights);
-		
+
 		JButton btnNewButton = new JButton("Search");
 		btnNewButton.addMouseListener(new MouseAdapter() {
+			@SuppressWarnings("deprecation")
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				try {
-						int numbernights = Integer.parseInt(textField_2.getText());
-						model.Date date = (model.Date) dp.getDate();
-						
-						ReservationService rs = new ReservationService();
-							List<House> houselist = rs.getAvailableHouses(date, numbernights);
-							for(House h : houselist){
-								unitsAvailable.addItem(h.toString());
-								}
-							
-							if(houselist.size() > 1){
-								JOptionPane.showMessageDialog(null, "No houses available");
-							}
-							else{
-								unitsAvailable.setEnabled(true);
-							}
-					}
-						
-					
-					
-					
-							
-				 catch (NumberFormatException e1) {
 					int numbernights = Integer.parseInt(textField_2.getText());
 					Date dpDate = dp.getDate();
 
@@ -119,6 +100,13 @@ public class Frame1 extends JFrame {
 					List<House> houselist = rs.getAvailableHouses(date, numbernights);
 					for (House h : houselist) {
 						unitsAvailable.addItem(h.toString());
+						}
+					
+					if(houselist.size() < 1){
+						JOptionPane.showMessageDialog(null, "No bungalows available.");
+					}
+					else{
+						unitsAvailable.setEnabled(true);
 					}
 				}
 
@@ -126,58 +114,61 @@ public class Frame1 extends JFrame {
 					// TODO Auto-generated catch block
 					// exception met delegate in ander scherm
 					e1.printStackTrace();
-				}
-				catch (Throwable ex) {
+				} catch (Throwable ex) {
 					ex.printStackTrace();
 				}
-				
-				
+
 			}
 		});
 		btnNewButton.setBounds(297, 67, 89, 23);
 		frame.getContentPane().add(btnNewButton);
-		
+
 		JLabel lblUnitsAvailable = new JLabel("Units Available");
 		lblUnitsAvailable.setBounds(26, 92, 93, 14);
 		frame.getContentPane().add(lblUnitsAvailable);
-		
+
 		unitsAvailable = new JComboBox<String>();
 		unitsAvailable.setEnabled(false);
 		unitsAvailable.setBounds(140, 89, 125, 20);
 		frame.getContentPane().add(unitsAvailable);
-		
+
 		JLabel lblLastNa = new JLabel("Last Name Tennant");
 		lblLastNa.setBounds(24, 137, 110, 14);
 		frame.getContentPane().add(lblLastNa);
-		
+
 		textField = new JTextField();
 		textField.setEnabled(false);
 		textField.setBounds(140, 134, 142, 20);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
-		
+
 		JLabel lblNewLabel = new JLabel("First name tennant");
 		lblNewLabel.setBounds(24, 172, 95, 14);
 		frame.getContentPane().add(lblNewLabel);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setEnabled(false);
 		textField_1.setColumns(10);
 		textField_1.setBounds(140, 169, 142, 20);
 		frame.getContentPane().add(textField_1);
-		
+
 		JButton btnNewButton_1 = new JButton("Register");
 		btnNewButton_1.setEnabled(false);
 		btnNewButton_1.setBounds(297, 209, 89, 23);
 		frame.getContentPane().add(btnNewButton_1);
-		
+
 		textField_2 = new JTextField();
 		textField_2.setBounds(138, 52, 86, 20);
 		frame.getContentPane().add(textField_2);
 		textField_2.setColumns(10);
-		
+
 		dp = new JDateChooser();
+		dp.setDateFormatString("dd/MM/yyyy");
 		dp.setBounds(140, 21, 125, 23);
 		frame.getContentPane().add(dp);
+		Date sysTime = new Date();
+		dp.setDate(sysTime);
+		
+		
 	}
 }
