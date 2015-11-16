8d3e2464141ac3e85de8b92b445029b51628c4f7
Pieter Huybrechts <huybrechtspieter@gmail.com>
2015-11-17 00:37:55 +0100

    Opdracht 3: View
    Created:
    -app: opent MainWindow
    -ButtonPanel: staan alle buttons in met listeners
    -RedPanel,BluePanel,YellowPanel: voorbeelden van toekomstige menupanels

--- Opdracht3/src/view/MainWindow.java
+++ Opdracht3/src/view/MainWindow.java
@@ -1,95 +1,58 @@
 package view;
 
-import java.awt.Color;
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import view.panels.*;
+
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.BoxLayout;
-import javax.swing.SwingConstants;
 
 public class MainWindow extends JFrame{
 
-	JPanel contentPanel;
-	JPanel buttonPanel;
-	JPanel viewPanel;
-	
-	JButton greenButton;
-	JButton blueButton;
-	JButton yellowButton;
-	
-	
-	public MainWindow(){
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private JPanel contentPanel;
+
+	public MainWindow() {
+		GridBagLayout gbl = new GridBagLayout();
+		gbl.columnWidths = new int[] { 200, 600 };
+		gbl.rowHeights = new int[] { 0 };
+		gbl.columnWeights = new double[] { Double.MIN_VALUE };
+		gbl.rowWeights = new double[] { Double.MIN_VALUE };
+
 		contentPanel = new JPanel();
-		contentPanel.setBackground(Color.WHITE);
-		buttonPanel = new JPanel();
-		buttonPanel.setBackground(Color.BLACK);
-		viewPanel = new JPanel();
-		viewPanel.setBackground(Color.GRAY);		
-		
-		greenButton=new JButton("Green Button");
-		greenButton.addActionListener(new GreenButtonListener());
-		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
-		buttonPanel.add(greenButton);
-		yellowButton=new JButton("Yellow Button");
-		yellowButton.addActionListener(new YellowButtonListener());
-		blueButton=new JButton("Blue Button");
-		blueButton.addActionListener(new BlueButtonListener());
-		buttonPanel.add(blueButton);
-		buttonPanel.add(yellowButton);
-		
+		contentPanel.setLayout(gbl);
+
+		setButtonPanel(new ButtonPanel(this));
+		setViewPanel(new BluePanel());
+
 		this.setContentPane(contentPanel);
-		this.setSize(800,600);
+		this.setSize(800, 600);
 		this.setLocationRelativeTo(null);
-		
+		this.setResizable(false);
+		this.setVisible(true);
+	}
+
+	public void setViewPanel(JPanel panel) {
+		GridBagConstraints gbc_viewPanel = new GridBagConstraints();
+		gbc_viewPanel.gridy = 0;
+		gbc_viewPanel.gridx = 1;
+		gbc_viewPanel.anchor = GridBagConstraints.CENTER;
+		gbc_viewPanel.fill = GridBagConstraints.BOTH;
+		contentPanel.add(panel, gbc_viewPanel);
+		this.setVisible(true);
+	}
+
+	private void setButtonPanel(ButtonPanel buttonPanel) {
 		GridBagConstraints gbc_buttonPanel = new GridBagConstraints();
 		gbc_buttonPanel.gridy = 0;
 		gbc_buttonPanel.gridx = 0;
+		gbc_buttonPanel.fill = GridBagConstraints.BOTH;
+		gbc_buttonPanel.anchor = GridBagConstraints.CENTER;
 		contentPanel.add(buttonPanel, gbc_buttonPanel);
-		contentPanel.add(viewPanel);
-		
-		GridBagLayout gbl_viewPanel = new GridBagLayout();
-		gbl_viewPanel.columnWidths = new int[] {200, 600};
-		gbl_viewPanel.rowHeights = new int[]{0};
-		gbl_viewPanel.columnWeights = new double[]{Double.MIN_VALUE};
-		gbl_viewPanel.rowWeights = new double[]{Double.MIN_VALUE};
-		contentPanel.setLayout(gbl_viewPanel);
-		
-		
-		this.setVisible(true);
 	}
-	
-	class GreenButtonListener implements ActionListener{
-
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			System.out.println("Green Button");			
-		}
-		
-	}
-	
-	class YellowButtonListener implements ActionListener{
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			System.out.println("Yellow Button");
-			
-		}
-
-	}
-	
-	class BlueButtonListener implements ActionListener{
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			System.out.println("Blue Button");			
-		}
-		
-	}
-	
 }
