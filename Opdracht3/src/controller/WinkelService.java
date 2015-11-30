7df7e8e82da218e1f7a168372804fe5f1e6d239f
Peter <Peter@10.25.100.201>
2015-11-30 21:33:06 +0100

    Interfaces gemaakt

---
+++ Opdracht3/src/controller/WinkelService.java
@@ -0,0 +1,34 @@
+package controller;
+
+import java.util.List;
+
+import common.enums.EnumTypeCd;
+import common.enums.EnumTypeDvd;
+import common.enums.EnumTypeGame;
+import model.Item;
+import model.subItems.Cd;
+import model.subItems.Dvd;
+import model.subItems.Games;
+
+public interface WinkelService {
+		
+	public class Winkeldummy {
+
+		public Winkeldummy() {
+			// TODO Auto-generated constructor stub
+		}
+
+	}
+
+	public void AddItem(Item item);
+	
+	public void RemoveItem (Item item);
+	
+	public List<Item> getAllItemsSorted() ;
+	
+	public List<Cd> searchCd(EnumTypeCd type, String searchString); 
+	
+	public List<Dvd> searchDvd(EnumTypeDvd type, String searchString); 
+	
+	public List<Games> searchGames(EnumTypeGame type, String searchString); 
+}
