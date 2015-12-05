33b2d1108dce364c2688599b414cb27c60a15620
Peter <Peter@192.168.0.163>
2015-12-05 15:22:44 +0100

    tijdelijk werk aan boeteservice

---
+++ Opdracht3/src/controller/BoeteController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import model.Uitlening;
+
+public class BoeteController implements BoeteService {
+
+	@Override
+	public BigDecimal berekenBoeteWaarde(Uitlening uitlening) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isErOpHuidigeUitleningEenBoete(Uitlening uitlening) {
+		return false;
+		// TODO Auto-generated method stub
+	}
+
+}
