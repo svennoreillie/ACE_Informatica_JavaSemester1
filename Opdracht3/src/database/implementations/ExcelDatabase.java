ae1b5acf3c61b24a6911c1a35074fd07bb2d7473
Sven Noreillie <sven.noreillie@gmail.com>
2015-12-19 15:24:03 +0100

    working on excel

--- Opdracht3/src/database/implementations/ExcelDatabase.java
+++ Opdracht3/src/database/implementations/ExcelDatabase.java
@@ -3,6 +3,8 @@ package database.implementations;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -10,6 +12,7 @@ import java.util.List;
 
 import common.DBException;
 import common.DBMissingException;
+import database.DataStrategy;
 import database.internalInterface.DataReadWriteService;
 import jxl.*;
 import jxl.read.biff.BiffException;
@@ -69,11 +72,11 @@ public class ExcelDatabase<T extends ModelBase> implements DataReadWriteService<
 			workbook.createSheet(className, 0);
 			WritableSheet sheet = workbook.getSheet(0);
 
-			Field[] genericFieldArray = this.classType.getDeclaredFields();
+			List<Method> genericFieldArray = getFields();
 
 			// Schrijf headers
 			int col = 0;
-			for (Field f : genericFieldArray) {
+			for (Method f : genericFieldArray) {
 				Label label = new Label(col, 0, f.getName());
 				sheet.addCell(label);
 				col++;
@@ -84,17 +87,31 @@ public class ExcelDatabase<T extends ModelBase> implements DataReadWriteService<
 			for (T item : list) {
 				col = 0;
 
-				for (Field f : genericFieldArray) {
+				for (Method f : genericFieldArray) {
 					Object value;
 					try {
-						value = f.get(item);
-
+						if (ModelBase.class.isAssignableFrom(f.getClass())) {
+							//Deze field is een modelbase op zich
+							//In dit geval moeten we de equivalent nemen
+							DataStrategy strategy = new DataStrategy(f.getClass());
+							ModelBase model = (ModelBase)f.invoke(item);
+							strategy.add(model);
+							//Save id of this item
+							value = model.getId();
+						} else {
+							value = f.invoke(item);
+						}
+						
 						if (value != null) {
 							Label label = new Label(col, row, value.toString());
 							sheet.addCell(label);
 						}
 					} catch (IllegalArgumentException | IllegalAccessException e) {
 						// stil afvangen
+						throw new DBException("", e);
+					} catch (InvocationTargetException e) {
+						// TODO Auto-generated catch block
+						throw new DBException("", e);
 					}
 
 					col++;
@@ -108,6 +125,25 @@ public class ExcelDatabase<T extends ModelBase> implements DataReadWriteService<
 		} catch (IOException | WriteException e) {
 			throw new DBException("Error writing to Excel database", e);
 		}
+	}
+
+	private List<Method> getFields() {
+		//Field[] genericFieldArray = this.classType.getDeclaredFields();
+		
+		List<Method> returnList = new ArrayList<Method>();
+		for (Method m : this.classType.getMethods()) {
+		    if (m.getName().startsWith("get") && m.getParameterTypes().length == 0) {
+		    	returnList.add(m);
+		    }
+		}
+		
+		
+//		List<Field> returnList = new ArrayList<Field>();
+//		for (Field field : genericFieldArray) {
+//			if (Iterable.class.isAssignableFrom(field.getDeclaringClass())) continue;
+//			returnList.add(field);
+//		}
+		return returnList;
 	}
 
 }
